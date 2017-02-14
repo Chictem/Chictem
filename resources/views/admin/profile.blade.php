47408854844de651c66d0e14b1b2243ad83b859f
Germey <cqc@cuiqingcai.com>
2017-02-14 16:45:53 +0800

    update profile

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -14,7 +14,7 @@
     <div style="background-size:cover; background: url({{ Voyager::image( Voyager::setting('admin_bg_image'), config('voyager.assets_path') . '/images/bg.jpg') }}) center center;position:absolute; top:0; left:0; width:100%; height:300px;"></div>
     <div style="height:160px; display:block; width:100%"></div>
     <div style="position:relative; z-index:9; text-align:center;">
-        <img src="{{ Voyager::image(Auth::user()->avatar) }}" class="avatar"
+        <img src="{{ Voyager::image(Auth::user()->image) }}" class="avatar"
              style="border-radius:50%; width:150px; height:150px; border:5px solid #fff;"
              alt="{{ Auth::user()->name }} avatar">
         <h4>{{ ucwords(Auth::user()->name) }}</h4>
